bf673d78ff1bffdf0191d229f544748e93b2d3be
Ian Ker-Seymer <i.kerseymer@gmail.com>
2021-01-07 23:01:57 -0500

    Simplify extconf.rb

--- ext/mini_phone/extconf.rb
+++ ext/mini_phone/extconf.rb
@@ -4,47 +4,6 @@
 
 require 'mkmf'
 
-LIBDIR      = RbConfig::CONFIG['libdir']
-
-INCLUDEDIR  = RbConfig::CONFIG['includedir']
-
-header_dirs = [
-  # First search /opt/local for macports
-  '/opt/local/include',
-
-  # Then search /usr/local for people that installed from source
-  '/usr/local/include',
-
-  # Check the ruby install locations
-  INCLUDEDIR,
-
-  # Finally fall back to /usr
-  '/usr/include'
-]
-
-lib_dirs = [
-  # First search /opt/local for macports
-  '/opt/local/lib',
-
-  # Then search /usr/local for people that installed from source
-  '/usr/local/lib',
-
-  # Check the ruby install locations
-  LIBDIR,
-
-  # Finally fall back to /usr
-  '/usr/lib'
-]
-
-# Detect homebrew installs
-if find_executable('brew')
-  brew_prefix = `brew --prefix`.strip
-  header_dirs.unshift "#{brew_prefix}/include"
-  lib_dirs.unshift "#{brew_prefix}/lib"
-end
-
-dir_config('mini_phone', header_dirs, lib_dirs)
-
 unless have_library('phonenumber')
   abort <<~MSG
 
@@ -69,6 +28,8 @@ unless have_library('phonenumber')
 
   MSG
 end
+
+dir_config('mini_phone')
 
 $CXXFLAGS += ' -std=c++11 -ofast '
 
